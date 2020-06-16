d522072a02c49cc66f8b8be2d0329e109e123547
Baavgai <brettmc@gmail.com>
2020-06-16 06:56:47 -0400

    best move

--- app/deps.ts
+++ app/deps.ts
@@ -1,1 +1,11 @@
 export * from "https://raw.githubusercontent.com/Baavgai/deno-funcs/master/graph2D/mod.ts";
+// export * from "https://raw.githubusercontent.com/Baavgai/deno-funcs/master/core/mod.ts";
+
+export const randInt = (n: number, endRange?: number): number =>
+  endRange === undefined
+    ? Math.floor(Math.random() * n)
+    : (n + randInt((endRange - n) + 1))
+  ;
+
+export const randPick = <T>(xs: T[]): T =>
+  xs[randInt(xs.length)];
--- app/tttEngine.ts
+++ app/tttEngine.ts
@@ -1,3 +1,5 @@
+import { randPick } from "./deps.ts";
+
 export const ROWS = 3, COLS = 3, CELL_COUNT = 9;
 
 export type EmptyCell = " ";
@@ -8,69 +10,71 @@ export type Board = readonly [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Ce
 
 
 export const EmptyBoard: Board =
-    [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
 
 export const stateInfo = (b: Board) => {
-    const playerCheck = (p: Player) =>
-        [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],
-            [0, 4, 8], [2, 4, 6]
-        ].some(([x, y, z]) => p === b[x] && p === b[y] && p === b[z]);
-    const allMoves = b.reduce((acc: number[], x, i) => x === " " ? [...acc, i] : acc, []);
-    const winner = playerCheck("O") ? "O" : playerCheck("X") ? "X" : " ";
-    const turn = CELL_COUNT - allMoves.length;
-    const done = (turn === CELL_COUNT) || (winner !== " ");
-    const playerTurn: Cell = done ? " " : ((turn % 2 === 0) ? "X" : "O");
-    const availableMoves = done ? [] : allMoves;
-    return {
-        availableMoves, winner, turn, done, playerTurn,
-        validMove: (pos: number) => availableMoves.some(x => x === pos)
-    };
+  const findWinningPositions = (p: Player) =>
+    [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ].find(([x, y, z]) => p === b[x] && p === b[y] && p === b[z]);
+  const win = findWinningPositions("O") || findWinningPositions("X");
+  const allMoves = b.reduce((acc: number[], x, i) => x === " " ? [...acc, i] : acc, []);
+  // const winner = playerCheck("O") ? "O" : playerCheck("X") ? "X" : " ";
+  const turn = CELL_COUNT - allMoves.length;
+  const done = (turn === CELL_COUNT) || (win !== undefined);
+  const winner: Cell | undefined = done ? (win ? b[win[0]] : " ") : undefined;
+
+  const playerTurn: Player | undefined = done ? undefined : ((turn % 2 === 0) ? "X" : "O");
+  const availableMoves = done ? [] : allMoves;
+  const validMove = (pos: number) => availableMoves.some(x => x === pos);
+  return { availableMoves, win, winner, turn, done, playerTurn, validMove };
 }
 
 
 
 export const print = (b: Board, nums = false) => {
-    const info = stateInfo(b);
-    const pv = (n: number, v: Cell) => " " + (nums ? (v === " " ? `${n}` : v) : v) + " ";
-    const p = (n: number) => pv(n, b[n]);
-    const pr = (x: number, y: number, z: number) => `${p(x)}|${p(y)}|${p(z)}`;
-    console.log(pr(0, 1, 2));
-    console.log("---+---+---");
-    console.log(pr(3, 4, 5));
-    console.log("---+---+---");
-    console.log(pr(6, 7, 8));
-    console.log(`Turn: ${info.turn + 1}`);
-    if (info.done) { console.log(info.winner === " " ? "Tie" : `Winner: ${info.winner}`); }
-    // console.log(info);
-    // console.log(`Turn: ${turn(b)}`);
-    // if (isDone(g)) { console.log(`Winner: ${g.winner}`); }
-    console.log();
+  const info = stateInfo(b);
+  const pv = (n: number, v: Cell) => " " + (nums ? (v === " " ? `${n}` : v) : v) + " ";
+  const p = (n: number) => pv(n, b[n]);
+  const pr = (x: number, y: number, z: number) => `${p(x)}|${p(y)}|${p(z)}`;
+  console.log(pr(0, 1, 2));
+  console.log("---+---+---");
+  console.log(pr(3, 4, 5));
+  console.log("---+---+---");
+  console.log(pr(6, 7, 8));
+  console.log(`Turn: ${info.turn + 1}`);
+  if (info.done) { console.log(info.winner === " " ? "Tie" : `Winner: ${info.winner}`); }
+  // console.log(info);
+  // console.log(`Turn: ${turn(b)}`);
+  // if (isDone(g)) { console.log(`Winner: ${g.winner}`); }
+  console.log();
 };
 
 
 const place = (b: Board, pos: number, value: Player): [Board, boolean] => {
-    const v = (i: number): Cell => i === pos ? value : b[i];
-    return !(stateInfo(b).validMove(pos))
-        ? [b, false]
-        : [[v(0), v(1), v(2), v(3), v(4), v(5), v(6), v(7), v(8)], true];
+  const v = (i: number): Cell => i === pos ? value : b[i];
+  return !(stateInfo(b).validMove(pos))
+    ? [b, false]
+    : [[v(0), v(1), v(2), v(3), v(4), v(5), v(6), v(7), v(8)], true];
 };
 
 
 export const move = (b: Board, pos: number): [Board, boolean] => {
-    const { playerTurn } = stateInfo(b);
-    return playerTurn === " " ? [b, false] : place(b, pos, playerTurn);
+  const { playerTurn } = stateInfo(b);
+  return playerTurn === undefined ? [b, false] : place(b, pos, playerTurn);
 };
 
 export const bestMove = (b: Board): number | undefined => {
-    const info = stateInfo(b);
-    if (info.done) {
-        return undefined;
-    } else if (info.turn === 0) {
-        return 4;
-    } else {
-        
-    }
-}
+  const info = stateInfo(b);
+  if (info.playerTurn) {
+    if (info.turn === 0) { return 4; }
+    const forPlayer = (player: Player) => info.availableMoves.find(pos => stateInfo(move(b, pos)[0]).winner === player);
+    return forPlayer(info.playerTurn)
+      || forPlayer(info.playerTurn === "X" ? "O" : "X")
+      || randPick(info.availableMoves);
+  }
+  return undefined;
+};
